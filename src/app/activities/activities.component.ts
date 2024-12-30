513f08a4c0686dc19c4cd38637f01c8d81262e03
AnderVegas <avegasfern15@gmail.com>
2024-12-30 16:32:44 +0100

    Prototype activities page and assets folder

--- src/app/activities/activities.component.ts
+++ src/app/activities/activities.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { DatepickerInlineCalendarExample } from '../activitie-calendar/activitie-calendar.component';
 
 @Component({
   selector: 'app-activities',
-  imports: [],
+  standalone: true,
+  imports: [DatepickerInlineCalendarExample],
   templateUrl: './activities.component.html',
   styleUrl: './activities.component.scss'
 })
